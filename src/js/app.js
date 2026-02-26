d08315502a3459ebfae8c43d02ddda65ef3a62e3
Luis <lalpizarbr@gmail.com>
2026-02-26 04:11:31 +0000

    nombre apellido profesion background

--- src/js/app.js
+++ src/js/app.js
@@ -33,8 +33,10 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
+          <h1>${variables.name == null ? "nombre" : variables.name} ${
+    variables.lastName == null ? "apellido" : variables.lastName
+  }</h1>
+          <h2>${variables.role == null ? "profesion" : variables.role}</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
@@ -54,7 +56,8 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://images.pexels.com/photos/943096/pexels-photo-943096.jpeg",
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (position-left or position-right)
